ddf036dbc77a69618410223402ac953a15e74f76
Jacob MacKinnon <tartvox@gmail.com>
2022-01-12 00:27:29 -0400

    Added News Source
    Now has BBC and CBC news

--- NewsApp/ContentView.swift
+++ NewsApp/ContentView.swift
@@ -44,26 +44,25 @@ struct StoryView: View {
     let urlToImage: String
     
     var body: some View {
-        //AsyncImage(url: URL(string: urlToImage))
         AsyncImage(url: URL(string: urlToImage)) { image in
             image.resizable()
         } placeholder: {
             ProgressView()
         }
         .frame(width: 250, height: 250)
-            VStack(alignment: .center) {
-                Text(title)
-                    .fontWeight(.bold)
-                    .font(.title2)
-                Text("Written By: \(author)")
-                    .font(.subheadline)
-                Text("\(description)")
-                    .font(.body)
-                    .padding(5)
-                Link("Continue Reading", destination: URL(string: url)!)
-                Spacer()
-                Spacer()
-                Spacer()
+        VStack(alignment: .center) {
+            Text(title)
+                .fontWeight(.bold)
+                .font(.title2)
+            Text("Written By: \(author)")
+                .font(.subheadline)
+            Text("\(description)")
+                .font(.body)
+                .padding(5)
+            Link("Continue Reading", destination: URL(string: url)!)
+            Spacer()
+            Spacer()
+            Spacer()
         }
     }
 }
@@ -71,10 +70,16 @@ struct StoryView: View {
 //82a5b077813e4ab18af51156ffe779f1
 //c2d3bd7de4b94226abe97ef19abd21a3
 //daf917ff9f654c36878582b201d54693
+//Sources https://newsapi.org/docs/endpoints/sources
+//BBC https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c2d3bd7de4b94226abe97ef19abd21a3
+//CBC https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=daf917ff9f654c36878582b201d54693
 struct ContentView: View {
+    @State private var apiNews = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c2d3bd7de4b94226abe97ef19abd21a3"
+    @State private var navTitle = "BBC | Top Stories"
+    
     @State private var articles = [Article]()
     func loadData() async {
-        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=82a5b077813e4ab18af51156ffe779f1") else {
+        guard let url = URL(string: apiNews) else {
             print("Invalid URL")
             return
         }
@@ -98,11 +103,11 @@ struct ContentView: View {
                         } placeholder: {
                             ProgressView()
                         }
-                        .frame(width: 255, height: 255)
+                        .frame(width: 260, height: 250)
                         .cornerRadius(10)
                         HStack {
                             VStack(alignment: .leading) {
-                                Text("Tech Stories")
+                                Text("Trending")
                                     .font(.headline)
                                     .foregroundColor(.secondary)
                                 Text(item.title)
@@ -131,7 +136,27 @@ struct ContentView: View {
                 .listRowBackground(Color.clear)
             }.task {
                 await loadData()
-            }.navigationTitle("Top Tech Stories")
+            }.navigationBarTitle(Text(navTitle), displayMode: .large)
+                .toolbar {
+                    Button("CBC") {
+                        navTitle = "CBC | Top Stories"
+                        apiNews = "https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=daf917ff9f654c36878582b201d54693"
+                        Task {
+                            await loadData()
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("BBC") {
+                            navTitle = "BBC | Top Stories"
+                            apiNews = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=daf917ff9f654c36878582b201d54693"
+                            Task {
+                                await loadData()
+                            }
+                        }
+                    }
+                }
         }
     }
 }
